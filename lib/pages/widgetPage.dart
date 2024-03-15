a2eb38432b1111b0330c22c97d454e0b06199523
Abdullah <abdullahlaptop6445@gmail.com>
2024-03-15 14:41:39 +0500

    widgetPage created

---
+++ lib/pages/widgetPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class widgetPage extends StatelessWidget {
+  const widgetPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
