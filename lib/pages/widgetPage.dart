198813cae70887e8cfe3fcfcddb1e933a11c52b3
Abdullah <abdullahlaptop6445@gmail.com>
2024-03-15 15:31:51 +0500

    created custom dopDown

--- lib/pages/widgetPage.dart
+++ lib/pages/widgetPage.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_important_widgets/pages/customDropDown.dart';
 
-class widgetPage extends StatelessWidget {
+class widgetPage extends StatefulWidget {
   const widgetPage({super.key});
 
   @override
+  State<widgetPage> createState() => _widgetPageState();
+}
+
+class _widgetPageState extends State<widgetPage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Important Widgets"),
+      ),
+      body: customDropDown(),
+    );
   }
 }
